4dabcb8db06b5e68f02ce6077ac265e34eb4330f
ChenShiZ <1206068807@qq.com>
2024-03-24 04:52:58 +0000

    python环境测试成功，可使用pytorch

--- testProject/test.py
+++ testProject/test.py
@@ -1,3 +1,10 @@
-import pytorch
+import numpy as np
+import torch # 如果pytorch安装成功即可导入
 
-print("python环境测试成功")
+
+print(torch.cuda.is_available()) # 查看CUDA是否可用
+print(torch.cuda.device_count()) # 查看可用的CUDA数量
+print(torch.version.cuda) # 查看CUDA的版本号
+print(torch.cuda.get_device_name(1))
+print(torch.cuda.get_device_capability(1))
+print(torch.cuda.get_device_properties(1))
